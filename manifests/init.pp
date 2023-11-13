86efb7db5b4da5b0344696674ec6032393368ce7
BasharDlale <bashar.dlaleh@gmail.com>
2023-11-13 11:02:01 +0200

    fix syntax errors

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,22 @@
 #   include microk8s
 class microk8s (
   $ipv4_address_cidr = '10.206.32.1/24',
+  $nodes = [{
+              vm_name      => 'master',
+              ipv4_address => '10.206.32.100',
+              memory       => '2GB',
+              disk         => '10GiB',
+              passwd       => '$1$SaltSalt$YhgRYajLPrYevs14poKBQ0',
+              master       => true
+             },
+             {
+              vm_name      => 'worker1',
+              ipv4_address => '10.206.32.101',
+              memory       => '2GB',
+              disk         => '10GiB',
+              passwd       => '$1$SaltSalt$YhgRYajLPrYevs14poKBQ0',
+              }],
+  $local_nfs_storage = false,
 ){
 
   file {'/tmp/lxd_init.yaml':
@@ -18,5 +34,10 @@ class microk8s (
   exec {'init':
     command => 'lxd init --preseed < /tmp/lxd_init.yaml',
     require => File['/tmp/lxd_init.yaml'],
+  }
+
+  class {'microk8s::instances':
+    nodes             => $nodes,
+    local_nfs_storage => false,
   }
 }
