1f531d0b021fdd09c43feeb7659351a75325e581
BasharDlale <bashar.dlaleh@gmail.com>
2023-11-14 16:34:50 +0200

    edit lxd launch script

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -38,15 +38,15 @@ define microk8s::vm (
     require => [File["/tmp/wait_${vm_name}.sh"], Exec["create_profile_${vm_name}"]],
   }
 
-#  exec {"wait_${vm_name}":
-#    command => "/tmp/wait_${vm_name}.sh",
-#    require => Exec["launch_${vm_name}"],
-#  }
+  exec {"wait_${vm_name}":
+    command => "/tmp/wait_${vm_name}.sh",
+    require => Exec["launch_${vm_name}"],
+  }
 
   if $master == false {
     exec {"microk8s-add-node_${vm_name}":
       command => "/snap/bin/lxc exec ${master_name} -- sudo microk8s add-node | grep 'microk8s join' | grep -v worker | head -1 > /tmp/microk8s-join 2>&1",
-#      require => Exec["wait_${vm_name}"],
+      require => Exec["wait_${vm_name}"],
     }
 
     exec {"microk8s-join-node_${vm_name}":
@@ -60,14 +60,14 @@ define microk8s::vm (
     $addons.each |$addon| {
       exec {"${vm_name}_${addon}":
         command => "/snap/bin/lxc exec ${master_name} -- sudo microk8s enable ${addon}",
- #       require => Exec["wait_${vm_name}"],
+        require => Exec["wait_${vm_name}"],
       }
     }
   }
 
   exec {"apt-update_${vm_name}":
     command => "/snap/bin/lxc exec ${vm_name} -- sudo apt update",
-  #  require => Exec["wait_${vm_name}"],
+    require => Exec["wait_${vm_name}"],
   }
 
   exec {"nfs-common_${vm_name}":
