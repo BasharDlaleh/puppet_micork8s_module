a2f2e75fec8a0ff639e3aea179a0312b2c9d101b
BasharDlale <bashar.dlaleh@gmail.com>
2023-11-15 15:31:45 +0200

    test

--- manifests/vm.pp
+++ manifests/vm.pp
@@ -69,13 +69,13 @@ define microk8s::vm (
 #    }
 #  }
 
-  exec {"apt-update_${vm_name}":
-    command => "/snap/bin/lxc exec ${vm_name} -- sudo apt update",
-    require => Exec["wait_${vm_name}"],
-  }
+#  exec {"apt-update_${vm_name}":
+#    command => "/snap/bin/lxc exec ${vm_name} -- sudo apt update",
+#    require => Exec["wait_${vm_name}"],
+#  }
 
-  exec {"nfs-common_${vm_name}":
-    command => "/snap/bin/lxc exec ${vm_name} -- sudo apt install nfs-common -y",
-    require => Exec["apt-update_${vm_name}"],
-  }
+#  exec {"nfs-common_${vm_name}":
+#    command => "/snap/bin/lxc exec ${vm_name} -- sudo apt install nfs-common -y",
+#    require => Exec["apt-update_${vm_name}"],
+#  }
 }
